eed5380151e44d324c747a050d42d67b947ecb69
David Carmichael <carmichaelits@gmail.com>
2023-09-14 15:57:14 +0100

    Super admin account creation added to seed cli command

--- app/global/cli/seed.py
+++ app/global/cli/seed.py
@@ -1,50 +1,63 @@
-import random
+import typing as t
+from getpass import getpass
 
+import click
 from flask import current_app as app
-from app.extensions import db
+
+from app.models import Resources
+from app.models.accounts import Accounts
+from app.models.roles import Roles
+from app.models.roles_membership import RolesMembership
 
 
-@app.cli.command("seed-test-data")
-def seed_test_data():
-    from app.models.accounts import Accounts
-    from app.models.roles import Roles
-    from app.models.roles_membership import RolesMembership
+@app.cli.command("seed")
+def seed():
+    def set_admin_account(email_address: t.Optional[str] = None):
+        if email_address is None:
+            _email_address = input("Enter super admin email: ")
+        else:
+            _email_address = email_address
 
-    accounts = [
-        {"email_address": "admin@sys.local", "password": "admin"},
-        {"email_address": "coc_person_one@sys.local", "password": "coc_person"},
-        {"email_address": "coc_person_two@sys.local", "password": "coc_person"},
-        {"email_address": "reviewer_one@sys.local", "password": "reviewer_one"},
-        {"email_address": "reviewer_two@sys.local", "password": "reviewer_two"},
-        {"email_address": "reviewer_three@sys.local", "password": "reviewer_three"},
-    ]
+        if Accounts.exists(_email_address):
+            print("Account already exists. Please enter another account.")
+            set_admin_account()
 
-    roles = [
-        {"name": "Administrator"},
-        {"name": "Code of Conduct Official"},
-        {"name": "Proposal Reviewer"},
-        {"name": "Speaker"},
-        {"name": "Sponsor"},
-        {"name": "Volunteer"},
-        {"name": "VIP Attendee"},
-        {"name": "Attendee"},
-    ]
+        _password = getpass("Enter super admin password: ")
+        _confirm_password = getpass("Confirm admin password: ")
 
-    if Accounts.__is_empty__():
-        print("Creating test accounts...")
-        Accounts.create_batch(accounts)
-    else:
-        print("Test accounts already exist.")
+        if _password != _confirm_password:
+            print("Passwords do not match. Please try again.")
+            set_admin_account(email_address=_email_address)
+
+        return _email_address, _password
 
     if Roles.__is_empty__():
-        print("Creating test roles...")
-        Roles.create_batch(roles)
+        print("Creating roles...")
+        Roles.create_batch(Resources.roles)
     else:
-        print("Test roles already exist.")
+        print("Roles table is not empty.")
 
-    # if RolesMembership.__is_empty__():
-    print("Assigning test roles...")
-    names = [x for x in map(lambda x: x["name"], roles)]
-    available_roles = Roles.get_by_name_batch(names)
+    print("creating super admin account...")
+    admin_email_address, admin_password = set_admin_account()
+    account_id = Accounts.create(
+        email_address=admin_email_address,
+        password=admin_password,
+        disabled=False,
+    )
 
-    print("Available roles:", available_roles)
+    admin_role_id = Roles.select_by_name("Super Administrator").role_id
+
+    RolesMembership.set_roles(account_id, [admin_role_id])
+
+    print("Done.", account_id, admin_role_id)
+
+
+@app.cli.command("does-account-exist")
+@click.option("--email-address", "-ea")
+def does_account_exist(email_address: str):
+    if email_address is None:
+        ea = input("Enter email address: ")
+    else:
+        ea = email_address
+
+    print(True if Accounts.exists(ea) else False)
