a30c2d8cd7d9977219a8aedfbe50dbee7dd9ca34
David Carmichael <carmichaelits@gmail.com>
2023-09-14 15:56:26 +0100

    name changes to classmethods to make them more understandable

--- app/models/roles.py
+++ app/models/roles.py
@@ -7,20 +7,31 @@ class Roles(db.Model, MetaMixins):
     role_id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(128), nullable=False)
 
+    rel_role_membership = relationship(
+        "RolesMembership",
+        order_by="RolesMembership.fk_account_id",
+        primaryjoin="Roles.role_id==RolesMembership.fk_role_id",
+        viewonly=True,
+    )
+
     @classmethod
-    def get_by_id(cls, role_id):
+    def select_by_id(cls, role_id):
         return db.session.execute(
             select(cls).filter_by(role_id=role_id).limit(1)
         ).scalar_one_or_none()
 
     @classmethod
-    def get_by_name(cls, name):
+    def select_by_name(cls, name):
         return db.session.execute(
             select(cls).filter_by(name=name).limit(1)
         ).scalar_one_or_none()
 
     @classmethod
-    def get_by_name_batch(cls, names: list[str]) -> dict[str, int]:
+    def select_names_batch(cls, names: list[str]) -> dict[str, int]:
+        """
+        Reutrns {name: role_id, name: role_id, ...}
+        """
+
         return {name: role_id for name, role_id in [
             (row.name, row.role_id) for row in db.session.execute(
                 select(cls).where(cls.name.in_(names))
@@ -28,7 +39,7 @@ class Roles(db.Model, MetaMixins):
         ]}
 
     @classmethod
-    def get_all(cls):
+    def select_all(cls):
         return db.session.execute(
             select(cls).order_by(cls.name)
         ).scalars().all()
