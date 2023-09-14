1862e6180d65ea22e6fbb0b986ec57dcb478372b
David Carmichael <carmichaelits@gmail.com>
2023-09-14 15:56:47 +0100

    relationships added

--- app/models/roles_membership.py
+++ app/models/roles_membership.py
@@ -6,7 +6,19 @@ class RolesMembership(db.Model, MetaMixins):
     fk_account_id = db.Column(db.Integer, db.ForeignKey("accounts.account_id"), nullable=False)
     fk_role_id = db.Column(db.Integer, db.ForeignKey("roles.role_id"), nullable=False)
 
-    rel_role = db.relationship("Roles", order_by="Roles.name")
+    rel_role = relationship(
+        "Roles",
+        order_by="Roles.name",
+        primaryjoin="RolesMembership.fk_role_id==Roles.role_id",
+        viewonly=True,
+    )
+
+    rel_account = relationship(
+        "Accounts",
+        order_by="Accounts.email_address",
+        primaryjoin="RolesMembership.fk_account_id==Accounts.account_id",
+        viewonly=True,
+    )
 
     @classmethod
     def get_by_account_id(cls, fk_account_id: int):
@@ -16,15 +28,15 @@ class RolesMembership(db.Model, MetaMixins):
         return result
 
     @classmethod
-    def set_roles(cls, fk_account_id: int, role_ids: list[int]):
+    def set_roles(cls, account_id: int, role_ids: list[int]):
         db.session.execute(
-            delete(cls).where(cls.fk_account_id == fk_account_id)
+            delete(cls).where(cls.fk_account_id == account_id)
         )
 
         for role_id in role_ids:
             db.session.execute(
                 insert(cls).values(
-                    fk_account_id=fk_account_id,
+                    fk_account_id=account_id,
                     fk_role_id=role_id
                 )
             )
