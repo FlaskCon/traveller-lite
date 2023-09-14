36a49cc6f966ffc49b68a695c447c5990271c3d0
David Carmichael <carmichaelits@gmail.com>
2023-09-14 15:53:32 +0100

    dataclass added to define roles

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,8 +1,34 @@
+from dataclasses import dataclass
+
 from sqlalchemy import select, update, delete, insert
 from sqlalchemy.orm import relationship
 from .__mixins__ import MetaMixins
 
 from app.extensions import db
+
+
+@dataclass
+class Resources:
+    roles = [
+        {"name": "Super Administrator"},
+        {"name": "Administrator"},
+        {"name": "Conference Chair"},
+        {"name": "Grants Chair"},
+        {"name": "Volunteer Chair"},
+        {"name": "Sprints Chair"},
+        {"name": "Speaker Support Chair"},
+        {"name": "Sponsorship Chair"},
+        {"name": "Diversity Chair"},
+        {"name": "Code of Conduct Official"},
+        {"name": "Proposal Reviewer"},
+        {"name": "Speaker"},
+        {"name": "Sponsor"},
+        {"name": "Volunteer"},
+        {"name": "VIP"},
+        {"name": "Attendee"},
+        {"name": "Emeritus"},
+    ]
+
 
 __all__ = [
     "db",
@@ -12,4 +38,5 @@ __all__ = [
     "insert",
     "relationship",
     "MetaMixins",
+    "Resources",
 ]
