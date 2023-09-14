34ecb61a741cd51e400136f7674b3e0ced43f4ee
David Carmichael <carmichaelits@gmail.com>
2023-09-14 15:55:06 +0100

    Additional account model queries and relationships

--- app/models/accounts.py
+++ app/models/accounts.py
@@ -8,20 +8,65 @@ class Accounts(db.Model, MetaMixins):
     salt = db.Column(db.String(4), nullable=False)
     disabled = db.Column(db.Boolean)
 
+    rel_roles = relationship(
+        "RolesMembership",
+        order_by="RolesMembership.fk_role_id",
+        primaryjoin="Accounts.account_id==RolesMembership.fk_account_id",
+        viewonly=True,
+    )
+
+    rel_profile = relationship(
+        "Profile",
+        order_by="Profile.profile_id",
+        primaryjoin="Accounts.account_id==Profile.fk_account_id",
+        viewonly=True,
+    )
+
     @classmethod
-    def get_all(cls):
+    def exists(cls, email_address):
+        return db.session.execute(
+            select(cls.account_id).filter_by(email_address=email_address).limit(1)
+        ).scalar_one_or_none()
+
+    @classmethod
+    def get_roles_from_email_address_select(cls, email_address):
+        result = db.session.execute(
+            select(cls).filter_by(email_address=email_address).limit(1)
+        ).scalar_one_or_none()
+        return [role.rel_role.name for role in result.rel_roles]
+
+    def get_roles_from_this(self):
+        return [role.rel_role.name for role in self.rel_roles]
+
+    @classmethod
+    def select_all(cls):
         return db.session.execute(
             select(cls).order_by(cls.email_address)
         ).scalars().all()
 
     @classmethod
-    def get_by_id(cls, account_id):
+    def select_using_id(cls, account_id):
         return db.session.execute(
             select(cls).filter_by(account_id=account_id).limit(1)
         ).scalar_one_or_none()
 
     @classmethod
+    def select_using_email_address(cls, email_address):
+        return db.session.execute(
+            select(cls).filter_by(email_address=email_address).limit(1)
+        ).scalar_one_or_none()
+
+    @classmethod
+    def select_account_id_using_email_address(cls, email_address):
+        return db.session.execute(
+            select(cls.account_id).filter_by(email_address=email_address).limit(1)
+        ).scalar_one_or_none()
+
+    @classmethod
     def create(cls, email_address, password, disabled):
+        """
+        Written for sqlite. Returns primary key after creation.
+        """
         from flask_imp.auth import Auth
 
         salt = Auth.generate_salt()
@@ -36,6 +81,8 @@ class Accounts(db.Model, MetaMixins):
             )
         )
         db.session.commit()
+
+        return cls.get_by_email_address(email_address).account_id
 
     @classmethod
     def update(
@@ -84,9 +131,7 @@ class Accounts(db.Model, MetaMixins):
         )
         db.session.commit()
 
-    def get_roles(self):
-        from .roles_membership import RolesMembership
-        return RolesMembership.get_by_account_id(self.account_id)
+    # batch actions:
 
     @classmethod
     def create_batch(cls, batch: list[dict]):
@@ -112,3 +157,9 @@ class Accounts(db.Model, MetaMixins):
             )
 
         db.session.commit()
+
+    @classmethod
+    def get_account_ids_from_email_address_select_batch(cls, email_addresses: list[str]) -> list[int]:
+        return db.session.execute(
+            select(cls.account_id).where(cls.email_address.in_(email_addresses))
+        ).scalars().all()
