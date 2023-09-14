e36cd6cd0348c1026303c2cf7bae985efe83631e
David Carmichael <carmichaelits@gmail.com>
2023-09-14 15:54:18 +0100

    Addition of a factory to control if development cli actions are available.

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,9 @@
+import os
+
 from flask import Flask
 from app.extensions import imp, db
+
+DEV_MODE = True if os.environ.get("DEV_MODE") else False
 
 
 def create_app():
@@ -8,10 +12,15 @@ def create_app():
     db.init_app(app)
     imp.import_models("models")
 
-    imp.import_app_resources()
+    imp.import_app_resources(
+        app_factories=["dev_cli"] if DEV_MODE else [],
+    )
     imp.import_blueprints("blueprints")
 
     with app.app_context():
         db.create_all()
+
+    if DEV_MODE:
+        print("DEV_MODE is enabled. Remember to turn this off in production (unset DEV_MODE)")
 
     return app
