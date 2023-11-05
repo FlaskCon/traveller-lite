6e1d408284b3f3ba5f664e1e8434677d6851ea9d
David <39418842+CheeseCake87@users.noreply.github.com>
2023-11-05 11:06:36 +0000

    Threading to Multiprocessing (#3)
    * Switched from Threading to Multiprocessing.
    
    Added better testing to check if the connection to the database is OK, and the connection to the email address is OK.
    
    Switching to Multiprocessing has added the ability to kill tasks. That has been added.
    
    * FIX: PROCESS is contained within REPROCESS anyway, no need to check for both.

--- app/extensions/emailer_client.py
+++ app/extensions/emailer_client.py
@@ -16,4 +16,16 @@ def start_emailer(database_uri: str, processor: Literal["REPROCESS", "PROCESS"] 
         try:
             s.send(send_message.encode("utf-8"), 8914)
         finally:
-            return s.recv(256)
+            return s.recv(256).decode("utf-8")
+
+
+def kill_process(pid: str):
+    with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as s:
+        try:
+            s.connect(SERVER_ADDRESS.name)
+        except socket.error as msg:
+            raise msg
+        try:
+            s.send(f"KILL~{pid}".encode("utf-8"), 8914)
+        finally:
+            return s.recv(256).decode("utf-8")
--- app/global/flask/cli_tools.py
+++ app/global/flask/cli_tools.py
@@ -1,3 +1,7 @@
+import os
+
+import click
+import faker
 from flask import current_app as app
 
 from app.extensions import db
@@ -8,3 +12,60 @@ def initdb_command():
     """Initializes the database."""
     db.create_all()
     print("Initialized the database.")
+
+
+@app.cli.command("email-test")
+def email_test_command():
+    from app.models.email_queue import EmailQueue
+
+    emails: list[dict[str, str]] = []
+
+    for r in range(10):
+        fake = faker.Faker()
+        emails.append({
+            "email_to": fake.email(),
+            "email_subject": fake.sentence(),
+            "email_message": fake.text(),
+        })
+
+    EmailQueue.add_emails_to_send(emails)
+    print(EmailQueue.process_queue())
+
+
+@app.cli.command("email-live-test")
+def email_live_test_command():
+    from app.models.email_queue import EmailQueue
+
+    emails: list[dict[str, str]] = []
+
+    fake = faker.Faker()
+
+    emails.append({
+        "email_to": os.getenv("LIVE_TEST_EMAIL_ONE", os.getenv("FLASKCON_EMAIL_ADDRESS")),
+        "email_subject": fake.sentence(),
+        "email_message": fake.text(),
+    })
+
+    emails.append({
+        "email_to": os.getenv("LIVE_TEST_EMAIL_TWO", os.getenv("FLASKCON_EMAIL_ADDRESS")),
+        "email_subject": fake.sentence(),
+        "email_message": fake.text(),
+    })
+
+    EmailQueue.add_emails_to_send(emails)
+    print(EmailQueue.process_queue())
+
+
+@app.cli.command("email-reprocess")
+def email_test_command():
+    from app.models.email_queue import EmailQueue
+
+    print(EmailQueue.reprocess_queue())
+
+
+@app.cli.command("email-kill")
+@click.argument("pid")
+def email_test_command(pid: str):
+    from app.extensions.emailer_client import kill_process
+
+    print(kill_process(pid))
