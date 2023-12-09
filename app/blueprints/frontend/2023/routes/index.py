2be15d8c9ba7b7454349dbaf7a3c4e0ab2d7a2c6
David <39418842+CheeseCake87@users.noreply.github.com>
2023-12-09 00:27:35 +0000

    Talk and workshop timings updated. (#44)
    * frontend style changes. Added media partners section.
    
    * fixes the header tags, fixed boats in mobile view - redesigned a little.
    
    * Talk and workshop timings updated.

--- app/blueprints/frontend/2023/routes/index.py
+++ app/blueprints/frontend/2023/routes/index.py
@@ -14,52 +14,64 @@ def index():
 
     talks = [
         {
-            "title": "Flask SocketIO Load Balancing",
-            "speaker": "Valentine Sean"
-        },
-        {
-            "title": "Insights Building Sizable Flask Sites",
-            "speaker": "Abdur-Rahmaan Janhangeer"
+            "title": "Configuring Flask-SQLAlchemy for Production Environments: Managing MultiDB Connections/Queries",
+            "speaker": "Paul Asalu",
+            "time": "Saturday 16th - 07:00 UTC"
         },
         {
             "title": "Building a Scalable Machine Learning API with Flask and TensorFlow",
-            "speaker": "Brayan Mwanyumba"
-        },
-        {
-            "title": "How is a Request Processed in Flask?",
-            "speaker": "Patrick Kennedy"
-        },
-        {
-            "title": "Optimizing VS Code for Flask",
-            "speaker": "Pamela Fox"
-        },
-        {
-            "title": "Building Federated GraphQL APIs using Flask",
-            "speaker": "Adarsh Divakaran"
-        },
-        {
-            "title": "Secure by Design: Building one time secret share application based on ReadOnce objects",
-            "speaker": "Shahriyar Rzayev"
-        },
-        {
-            "title": "Building Web Applications with Flask and SQLAlchemy",
-            "speaker": "Aman Singh"
-        },
-        {
-            "title": "Configuring Flask-SQLAlchemy for Production Environments: Managing MultiDB Connections/Queries",
-            "speaker": "Paul Asalu"
-        },
-        {
-            "title": "Quart in Action: Async APIs for Model Front-Ends",
-            "speaker": "Adam Englander"
+            "speaker": "Brayan Mwanyumba",
+            "time": "Saturday 16th - 08:00 UTC"
         },
         {
             "title": "Don't think about settings, use Dynaconf",
-            "speaker": "Bruno Rocha"
+            "speaker": "Bruno Rocha",
+            "time": "Saturday 16th - 10:00 UTC"
+        },
+        {
+            "title": "How is a Request Processed in Flask?",
+            "speaker": "Patrick Kennedy",
+            "time": "Saturday 16th - 15:00 UTC"
         },
         {
             "title": "End-to-End Flask App Testing with Playwright",
-            "speaker": "Jay Miller"
+            "speaker": "Jay Miller",
+            "time": "Saturday 16th - 16:00 UTC"
+        },
+        {
+            "title": "Optimizing VS Code for Flask",
+            "speaker": "Pamela Fox",
+            "time": "Saturday 16th - 17:00 UTC"
+        },
+        {
+            "title": "Flask SocketIO Load Balancing",
+            "speaker": "Valentine Sean",
+            "time": "Saturday 16th - 18:00 UTC"
+        },
+        {
+            "title": "Quart in Action: Async APIs for Model Front-Ends",
+            "speaker": "Adam Englander",
+            "time": "Saturday 16th - 19:00 UTC"
+        },
+        {
+            "title": "Secure by Design: Building one time secret share application based on ReadOnce objects",
+            "speaker": "Shahriyar Rzayev",
+            "time": "Sunday 17th - 14:00 UTC"
+        },
+        {
+            "title": "Insights Building Sizable Flask Sites",
+            "speaker": "Abdur-Rahmaan Janhangeer",
+            "time": "Time Unconfirmed"
+        },
+        {
+            "title": "Building Federated GraphQL APIs using Flask",
+            "speaker": "Adarsh Divakaran",
+            "time": "Time Unconfirmed"
+        },
+        {
+            "title": "Building Web Applications with Flask and SQLAlchemy",
+            "speaker": "Aman Singh",
+            "time": "Time Unconfirmed"
         },
     ]
 
