74750c4bfec3f86103f858d467688fc59df0bcf7
David Carmichael <carmichaelits@gmail.com>
2024-03-22 18:45:37 +0000

    fix: change name of days_until_cfp_ends filter to days_left to be more generic

--- app/resources/flask/filters.py
+++ app/resources/flask/filters.py
@@ -20,20 +20,22 @@ def replace_htt_for_https(value: str) -> str:
     return value
 
 
-@app.template_filter("days_until_cfp_ends")
-def days_until_cfp_ends(
-    call_for_proposals_end_date: Optional[Union[datetime, date]],
+@app.template_filter("days_left")
+def days_left(
+        date_: Optional[Union[datetime, date]],
 ) -> int:
     """
-    Returns the number of days until the CFP ends.
-    """
-    if isinstance(call_for_proposals_end_date, date):
-        now = DatetimeDeltaMC()
-        return (call_for_proposals_end_date - now.date).days
+    Returns the number of days left to get to the given date.
 
-    if isinstance(call_for_proposals_end_date, datetime):
+    Used to calculate the number of days left for the CFP.
+    """
+    if isinstance(date_, date):
         now = DatetimeDeltaMC()
-        return (call_for_proposals_end_date.date() - now.date).days
+        return (date_ - now.date).days
+
+    if isinstance(date_, datetime):
+        now = DatetimeDeltaMC()
+        return (date_.date() - now.date).days
 
     return 0
 
