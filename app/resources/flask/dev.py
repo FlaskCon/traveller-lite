c8b6c611e8fec536310d415f8aa6962065383bcb
David Carmichael <carmichaelits@gmail.com>
2024-03-23 10:52:36 +0000

    feat: add ability for the dev-seed command to work with multiple years, and add test proposal data.

--- app/resources/flask/dev.py
+++ app/resources/flask/dev.py
@@ -12,6 +12,7 @@ def dev_cli(passed_app):
     from app.models.accounts import Accounts
     from app.models.roles import Roles
     from app.models.roles_membership import RolesMembership
+    from app.models.proposals import Proposals
 
     fake = Faker()
     app: Flask = passed_app
@@ -19,6 +20,7 @@ def dev_cli(passed_app):
     @app.cli.command("dev-seed")
     def dev_seed_test_data():
         random_accounts = 10
+        years = [2023, 2024]
 
         if Roles.__is_empty__():
             print("Creating roles from Resources.roles...")
@@ -38,7 +40,7 @@ def dev_cli(passed_app):
         else:
             print("Proposal statuses table is not empty.")
 
-        print(f"Generating {random_accounts} random accounts...")
+        print(f"Generating {random_accounts} random accounts, some with proposals...")
         random_amount_of_attendees = []
         random_attendee_type = [["Attendee"], ["VIP", "Attendee"]]
         for i in range(1, random_accounts):
@@ -47,7 +49,7 @@ def dev_cli(passed_app):
                 {
                     "email_address": fake.email(),
                     "name_or_alias": fake.name(),
-                    "password": "password",
+                    "password": "!password123",
                     "roles": roles,
                 }
             )
@@ -56,43 +58,43 @@ def dev_cli(passed_app):
             {
                 "email_address": "admin@sys.local",
                 "name_or_alias": "Administrator",
-                "password": "password",
+                "password": "!password123",
                 "roles": ["Administrator"],
             },
             {
                 "email_address": "cocofficial@sys.local",
                 "name_or_alias": "Code of Conduct Official",
-                "password": "password",
+                "password": "!password123",
                 "roles": ["Code of Conduct Official"],
             },
             {
                 "email_address": "reviewer@sys.local",
                 "name_or_alias": "Proposal Reviewer",
-                "password": "password",
+                "password": "!password123",
                 "roles": ["Proposal Reviewer"],
             },
             {
                 "email_address": "speaker@sys.local",
                 "name_or_alias": "Speaker",
-                "password": "password",
+                "password": "!password123",
                 "roles": ["Speaker"],
             },
             {
                 "email_address": "sponsor@sys.local",
                 "name_or_alias": "Sponsor",
-                "password": "password",
+                "password": "!password123",
                 "roles": ["Sponsor"],
             },
             {
                 "email_address": "volunteer@sys.local",
                 "name_or_alias": "Volunteer",
-                "password": "password",
+                "password": "!password123",
                 "roles": ["Volunteer"],
             },
             {
                 "email_address": "attendee@sys.local",
                 "name_or_alias": "Attendee",
-                "password": "password",
+                "password": "!password123",
                 "roles": ["Attendee"],
             },
         ]
@@ -101,16 +103,54 @@ def dev_cli(passed_app):
             [*defined_accounts, *random_amount_of_attendees], confirm_accounts=True
         )
 
-        for account in [*defined_accounts, *random_amount_of_attendees]:
-            account_id = Accounts.select_account_id_using_email_address(
-                account.get("email_address")
+        for defined_account in defined_accounts:
+            defined_account_id = Accounts.select_account_id_using_email_address(
+                defined_account.get("email_address")
             )
-            if account_id is None:
-                raise Exception(f"Account not found: {account.get('email_address')}")
+            if defined_account_id is None:
+                raise Exception(
+                    f"Account not found: {defined_account.get('email_address')}"
+                )
 
-            list_of_roles = account.get("roles", [])
+            list_of_roles = defined_account.get("roles", [])
             role_ids: list[int] = list(Roles.select_names_batch(list_of_roles).values())
-            RolesMembership.set_roles(account_id, role_ids)
+            RolesMembership.set_roles(defined_account_id, role_ids)
+
+        for attendee_account in random_amount_of_attendees:
+            attendee_account_id = Accounts.select_account_id_using_email_address(
+                attendee_account.get("email_address")
+            )
+            if attendee_account_id is None:
+                raise Exception(
+                    f"Account not found: {attendee_account.get('email_address')}"
+                )
+
+            list_of_roles = attendee_account.get("roles", [])
+            role_ids: list[int] = list(Roles.select_names_batch(list_of_roles).values())
+            RolesMembership.set_roles(attendee_account_id, role_ids)
+
+            detail = fake.text()
+            abstract = fake.text()
+            short_biography = fake.text()
+            notes_or_requests = fake.text()
+
+            tags = ",".join(fake.words(nb=3))
+
+            Proposals.submit_new_proposal(
+                fk_account_id=attendee_account_id,
+                title=fake.text(),
+                detail=detail,
+                detail_markdown=f"<p>{detail}</p>",
+                abstract=abstract,
+                abstract_markdown=f"<p>{abstract}</p>",
+                speaker_name=fake.name(),
+                short_biography=short_biography,
+                short_biography_markdown=f"<p>{short_biography}</p>",
+                notes_or_requests=notes_or_requests,
+                notes_or_requests_markdown=f"<p>{notes_or_requests}</p>",
+                tags=tags.replace(" ", ""),
+                year=random.choice(years),
+            )
 
     @app.cli.command("dev-show-accounts")
     def dev_show_accounts():
