457aa272d36e0a8eaa72b839fe77b6940ea61a69
David <carmichaelits@gmail.com>
2025-02-20 21:07:56 +0000

    update blueprint

--- app/2024/__init__.py
+++ app/2024/__init__.py
@@ -1,5 +1,11 @@
-from flask_imp import Blueprint
+from flask_imp import ImpBlueprint
+from flask_imp.config import ImpBlueprintConfig
 
-bp = Blueprint(__name__)
+bp = ImpBlueprint(__name__, ImpBlueprintConfig(
+    enabled=True,
+    url_prefix="/2024",
+    static_folder="static",
+    template_folder="templates",
+))
 
 bp.import_resources("routes")
