72ef19064d864324cb2cbb4ecb7bd5b23be7d65d
David <carmichaelits@gmail.com>
2025-02-21 11:52:27 +0000

    redirect index to coming soon

--- app/conf_2025/__init__.py
+++ app/conf_2025/__init__.py
@@ -11,7 +11,7 @@ conf_2025 = Blueprint(
 
 @conf_2025.route("/", methods=["GET"])
 def index():
-    return render_template("conf_2025/index.html")
+    return redirect(url_for("conf_2025.coming_soon"))
 
 
 @conf_2025.route("/coming-soon", methods=["GET"])
