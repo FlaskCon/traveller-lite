92b370e4e3b33649042911cd0e52dc1ad93cb55a
David <carmichaelits@gmail.com>
2025-02-21 08:38:45 +0000

    add js router to include coming soon page

--- frontend_2024/Router.jsx
+++ frontend_2024/Router.jsx
@@ -1,6 +1,8 @@
 /* @refresh reload */
 import {render} from 'solid-js/web'
+import {Route, Router, Routes} from "@solidjs/router";
 import Index from './index.jsx'
+import ComingSoon from "./ComingSoon";
 
 const root = document.getElementById('root')
 
@@ -9,4 +11,11 @@ if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
         'to add it to your index.html? Or maybe the id attribute got misspelled?')
 }
 
-render(() => Index, root)
+render(() => (
+    <Router>
+        <Routes>
+            <Route path="/2024" component={Index}/>
+            <Route path="/2024/coming-soon" component={ComingSoon}/>
+        </Routes>
+    </Router>
+), root)
