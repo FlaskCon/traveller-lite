e41b8259a5c451096eade4bc9426f61bedbee5cd
David <carmichaelits@gmail.com>
2025-02-21 08:41:12 +0000

    conf_2025 setup

--- app/conf_2025/__init__.py
+++ app/conf_2025/__init__.py
@@ -1,39 +1,39 @@
 from flask import Blueprint, render_template, redirect, url_for
 
-conf_2024 = Blueprint(
-    "conf_2024",
+conf_2025 = Blueprint(
+    "conf_2025",
     __name__,
-    url_prefix="/2024",
+    url_prefix="/2025",
     static_folder="static",
     template_folder="templates"
 )
 
 
-@conf_2024.route("/", methods=["GET"])
+@conf_2025.route("/", methods=["GET"])
 def index():
-    return render_template("conf_2024/index.html")
+    return render_template("conf_2025/index.html")
 
 
-@conf_2024.route("/coming-soon", methods=["GET"])
+@conf_2025.route("/coming-soon", methods=["GET"])
 def coming_soon():
-    return render_template("conf_2024/coming-soon.html")
+    return render_template("conf_2025/coming-soon.html")
 
 
-@conf_2024.get("/code-of-conduct")
+@conf_2025.get("/code-of-conduct")
 def code_of_conduct():
-    return redirect(url_for("code_of_conduct", f="conf_2024"))
+    return redirect(url_for("code_of_conduct", f="conf_2025"))
 
 
-@conf_2024.get("/privacy-policy")
+@conf_2025.get("/privacy-policy")
 def privacy_policy():
-    return redirect(url_for("privacy_policy", f="conf_2024"))
+    return redirect(url_for("privacy_policy", f="conf_2025"))
 
 
-@conf_2024.get("/speaking-experience")
+@conf_2025.get("/speaking-experience")
 def speaking_experience():
-    return redirect(url_for("speaking_experience", f="conf_2024"))
+    return redirect(url_for("speaking_experience", f="conf_2025"))
 
 
-@conf_2024.get("/become-a-sponsor")
+@conf_2025.get("/become-a-sponsor")
 def become_a_sponsor():
-    return redirect(url_for("become_a_sponsor", f="conf_2024"))
+    return redirect(url_for("become_a_sponsor", f="conf_2025"))
