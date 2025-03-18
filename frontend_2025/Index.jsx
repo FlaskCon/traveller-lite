62b33daea9b23599aa964dd866f1aea5f61d117f
David <carmichaelits@gmail.com>
2025-03-18 20:56:38 +0000

    move overflow to main to enable the video background to stay in window when scrolling

--- frontend_2025/Index.jsx
+++ frontend_2025/Index.jsx
@@ -7,58 +7,62 @@ export default function Index() {
 
     return (
         <>
+            <video autoPlay muted loop id="bg-video">
+
+                <source src={stars_background} type="video/mp4"/>
+
+            </video>
             <main>
-                <section className={"logo-container"}>
+                <div className={"container"}>
+                    <section className={"logo-container"}>
 
-                    <img src={flaskcon_logo_animated} alt="FlaskCon 2025 Logo."/>
-                    <img src={pycon_2025_logo} className={"pyconus-logo"} alt="pycon2025 Logo."/>
+                        <img src={flaskcon_logo_animated} alt="FlaskCon 2025 Logo."/>
+                        <img src={pycon_2025_logo} className={"pyconus-logo"} alt="pycon2025 Logo."/>
 
-                </section>
+                    </section>
 
-                <section className={"text-center p-5"}>
-                    <h1>Friday May 16, 2 PM -6 PM</h1>
-                    <p className={"my-2"}>Inside PyCon US</p>
-                </section>
+                    <section className={"text-center p-5"}>
+                        <h1>Friday May 16, 2 PM -6 PM</h1>
+                        <p className={"my-2"}>Inside PyCon US</p>
+                    </section>
 
-                <section className={"text-center p-2"}>
-                    <p>Join us in a mini conference dedicated to Flask,
-                        its community and ecosystem, as well as related
-                        web technologies. Meet maintainers and community
-                        members, learn about how to get involved,
-                        and join us during the sprint days to contribute.
-                        Submit your talk proposal today!</p>
+                    <section className={"text-center p-2"}>
+                        <p>Join us in a mini conference dedicated to Flask,
+                            its community and ecosystem, as well as related
+                            web technologies. Meet maintainers and community
+                            members, learn about how to get involved,
+                            and join us during the sprint days to contribute.
+                            Submit your talk proposal today!</p>
 
-                    <a className={"a-button my-10"}
-                       href={"https://www.papercall.io/flaskcon-pyconus2025"}>
+                        <a className={"a-button my-10"}
+                           href={"https://www.papercall.io/flaskcon-pyconus2025"}>
 
-                        Submit a talk
+                            Submit a talk
 
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none"
-                             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                             className="lucide lucide-square-arrow-out-up-right">
-                            <path d="M21 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h6"/>
-                            <path d="m21 3-9 9"/>
-                            <path d="M15 3h6v6"/>
-                        </svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"
+                                 fill="none"
+                                 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                                 className="lucide lucide-square-arrow-out-up-right">
+                                <path d="M21 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h6"/>
+                                <path d="m21 3-9 9"/>
+                                <path d="M15 3h6v6"/>
+                            </svg>
 
-                    </a>
+                        </a>
 
-                    <p>
-                        CFP closes at April 17, 2025 00:00 UTC
-                    </p>
+                        <p>
+                            CFP closes at April 17, 2025 00:00 UTC
+                        </p>
 
-                </section>
+                    </section>
 
 
-                <video autoPlay muted loop id="bg-video">
-
-                    <source src={stars_background} type="video/mp4"/>
-
-                </video>
+                    <footer className={"text-center text-xs py-20"}>
+                        Copyright © 2024 FlaskCon, A PSF-registered trademark owned by Pallets.
+                    </footer>
+                </div>
             </main>
-            <footer className={"text-center text-xs py-10"}>
-                Copyright © 2024 FlaskCon, A PSF-registered trademark owned by Pallets.
-            </footer>
+
         </>
     )
 
