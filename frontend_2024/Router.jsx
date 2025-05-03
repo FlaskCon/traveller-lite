69b7d37ad9d4f82c4fbe91ff46e419ae59e40cde
David <carmichaelits@gmail.com>
2025-05-03 22:44:05 +0100

    add redirect to frontends

--- frontend_2024/Router.jsx
+++ frontend_2024/Router.jsx
@@ -1,6 +1,6 @@
 /* @refresh reload */
 import {render} from 'solid-js/web'
-import {Route, Router, Routes} from "@solidjs/router";
+import {Navigate, Route, Router, Routes} from "@solidjs/router";
 import Index from './index.jsx'
 import ComingSoon from "./ComingSoon.jsx";
 
@@ -11,9 +11,14 @@ if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
         'to add it to your index.html? Or maybe the id attribute got misspelled?')
 }
 
+function Redirect() {
+    return <Navigate href={`/2024`}/>
+}
+
 render(() => (
     <Router>
         <Routes>
+            <Route path="/" component={Redirect}/>
             <Route path="/2024" component={Index}/>
             <Route path="/2024/coming-soon" component={ComingSoon}/>
         </Routes>
--- frontend_2025/Router.jsx
+++ frontend_2025/Router.jsx
@@ -1,6 +1,6 @@
 /* @refresh reload */
 import {render} from 'solid-js/web'
-import {Route, Router, Routes} from "@solidjs/router";
+import {Navigate, Route, Router, Routes} from "@solidjs/router";
 import Index from './index.jsx'
 import ComingSoon from "./ComingSoon.jsx";
 
@@ -11,9 +11,14 @@ if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
         'to add it to your index.html? Or maybe the id attribute got misspelled?')
 }
 
+function Redirect() {
+    return <Navigate href={`/2025`}/>
+}
+
 render(() => (
     <Router>
         <Routes>
+            <Route path="/" component={Redirect}/>
             <Route path="/2025" component={Index}/>
             <Route path="/2025/coming-soon" component={ComingSoon}/>
         </Routes>
